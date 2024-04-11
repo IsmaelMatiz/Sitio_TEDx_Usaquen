6191032b1d769577b9f26054c42de9b70c4909cc
IsmaelMatiz <ismaelmatiz@gmail.com>
2024-04-11 12:10:42 -0500

    first commit initial setup dependencies, structure, etc

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import { HomeScreen } from './pages/home';
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <HomeScreen/>
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+
+export function HomeScreen(){
+    return(
+        <>
+            <h1>Pantalla de inicio</h1>
+            <p>nada</p>
+            <button class="btn btn-primary">Primary button</button>
+        </>
+    )
+}
