e8b4335c6db49633ca1d62a9ee14ec466706a0d8
IsmaelMatiz <ismaelmatiz@gmail.com>
2024-06-08 12:40:54 -0500

    removed unnecesary number

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import { HomeScreen } from './pages/home';
+import './components/CSS/general.scss'
 
 const router = createBrowserRouter([
   {
